76075640b002670ad72a8ac48a45e5a27ef09832
Ratkovski <drezcry@hotmail.com>
2022-09-25 20:03:18 -0300

    add button

--- FRONTEND/dio/src/App.js
+++ FRONTEND/dio/src/App.js
@@ -1,10 +1,23 @@
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-Olá React
+
+      <h1>Olá React</h1>
+      <Button title="salvar"/>
+
+
+
+
+
+      
+
     </div>
   );
 }
 
 export default App;
+
+
+
